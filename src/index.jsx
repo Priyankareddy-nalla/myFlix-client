1fd97ec918d242f2cd0386f86ee0735ea8e2a5d8
Priyankareddy-nalla <148527465+Priyankareddy-nalla@users.noreply.github.com>
2024-03-09 14:07:25 +0100

    Applying React-Bootstrap
    applying react bootstrap to myFlix app

--- src/index.jsx
+++ src/index.jsx
@@ -2,15 +2,27 @@ import { createRoot } from 'react-dom/client';
 
 import { MainView } from "./components/main-view/main-view";
 
+import { Container } from 'react-bootstrap';
+
 
 
 // Import statement to indicate that you need to bundle `./index.scss`
 import "./index.scss";
 
+
+// {
+//   /* The following line can be included in your src/index.js or App.js file */
+// }
+// import 'bootstrap/dist/css/bootstrap.min.css';
+
 // Main component (will eventually use all the others)
 const MyFlixApplication = () => {
-  return <MainView />;
-};
+  return (
+    // <Container style = {{border: "1px solid red"}}>
+    <Container>
+  <MainView />
+   </Container>
+)};
 
 // Finds the root of your app
 const container = document.querySelector("#root");
